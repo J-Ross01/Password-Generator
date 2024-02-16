6c2004513635d2cddf257ed3f799fd195daa4c11
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-02-15 20:41:32 -0800

    added constant variables and let declarations.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+function generatePassword() {
 
+  const lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberCharacters = '0123456789';
+  const specialCharacters = '!@#$%^&*()_+{}:"<>?|[];\'.,/`~\\-=';
+
+  let length = parseInt(prompt("What is the length of the password? (8-128)"));
+  let includeLowercase = confirm("Would you like to include lowercase characters?");
+  let includeUppercase = confirm("Would you like to include uppercase characters?");
+  let includeNumbers = confirm("Would you like to include Numbers?");
+  let includeSpecial = confirm("Would you like to include Special characters?");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
